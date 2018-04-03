dcbb81026993dc9eb24e77f37092a5037473c7f0
Andrew <jaknowles1997@gmail.com>
2018-04-03 09:27:00 -0700

    created api route and logic to find best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,40 @@
 var path = require("path");
-var friends = require("/friends.js");
+var friends = require("../data/friends.js");
 
-exports = function(app) {
-    app.get("/api/friends", function() {
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
 
-    app.post("/api/friends" , function() {
+    app.post("/api/friends", function(req, res) {
+        var newUser = req.body;
+        var userScores = newUser.scores
+        var difference;
+        var bestMatch = {
+            name: "",
+            img: "",
+            totalDiff: 999
+        }
+        // go through every friend
+        for(var i=0; i < friends.length; i++) {
+            difference = 0;
+            console.log(friends[i].name);
+            // current incomming answer - current friend answer
+           for(var j=0; j < friends[i].scores[j]; j++) {
+                difference += Math.abs(parseInt(newUser.scores[j]) - parseInt(friends[i].scores[j]));
 
+            }
+            if(difference <= bestMatch.totalDiff) {
+                bestMatch.name = friends[i].name;
+                bestMatch.img = friends[i].img;
+                bestMatch.totalDiff = difference;
+            }
+
+        }
+
+        // add the new user to friends array and return their new couch comrade 
+        friends.push(newUser)
+        res.json(bestMatch)
+        console.log(bestMatch);
     });
 }
